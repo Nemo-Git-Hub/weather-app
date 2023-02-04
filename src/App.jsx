c0929141b7bf60fb2746d10366ed5c9065be57e0
0leh-Kolesnyk <oleh.kolesnyk.dnepr@gmail.com>
2023-02-04 03:55:41 +0200

    Created Weather APP v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-// require("dotenv").config();
-// import * as dotenv from "dotenv";
-// dotenv.config();
-// import express from "express";
-// console.log(process.env);
 import { useState, useEffect } from "react";
-// import Weather from "./components/weather";
+
+import Weather from "./components/Weather";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [lat, setLat] = useState([]); //широта
-  const [long, setLong] = useState([]); //долгота
-  const [data, setData] = useState([]); //объект данных погоды
+  const [lat, setLat] = useState(null); //широта
+  const [long, setLong] = useState(null); //долгота
+  const [data, setData] = useState(null); //объект данных погоды
 
   useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+  }, []);
 
+  useEffect(() => {
+    if (!lat || !long) return;
+    const fetchData = async () => {
       await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&APPID=0b9ac3cd89746404e521d90f5f6a0688`
+        `${
+          import.meta.env.VITE_API_URL
+        }/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${
+          import.meta.env.VITE_API_KEY
+        }&lang=ru`
       )
-        // await fetch(
-        //   `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-        // )
         .then((res) => res.json())
         .then((result) => {
           setData(result);
@@ -33,25 +33,13 @@ function App() {
       currentCount;
     };
     fetchData();
-
-    console.log("Latitude is:", lat); //широта
-    console.log("Longitude is:", long); //долгота
-    console.log(count);
   }, [lat, long]);
 
   const currentCount = () => setCount((prevCount) => prevCount + 1);
 
   return (
-    <div className="bg-[#ea6666] text-white">
-      {/* <button className="border-2 text-2xl" onClick={currentCount}>
-        count is {count}
-      </button> */}
-
-      {typeof data.main != "undefined" ? (
-        <Weather weatherData={data} />
-      ) : (
-        <div></div>
-      )}
+    <div className="bg-gray-900 text-gray-200">
+      {data ? <Weather data={data} /> : <div></div>}
     </div>
   );
 }
---
+++ src/components/Weather.jsx
@@ -0,0 +1,57 @@
+import { DateTime } from "luxon";
+
+function Weather({ data }) {
+  const currentDateTime = DateTime.local().toLocaleString(DateTime.DATE_HUGE);
+  console.log(currentDateTime);
+  return (
+    <>
+      <div className="bg-gray-800 p-4 rounded-lg w-max  ">
+        <div className="bg-gray-700 p-2 m-2 rounded-lg">
+          <div className="bg-gray-600 p-2 m-2 rounded-lg text-2xl font-semibold italic">
+            {currentDateTime}
+          </div>
+          <div className="bg-gray-600 p-2 m-2 rounded-lg">{data.name}</div>
+        </div>
+        <div className="bg-gray-700 p-2 m-2 rounded-lg">
+          <div className="bg-gray-600 p-2 m-2 rounded-lg text-6xl text-center">
+            {Math.round(data.main.temp)} °C
+          </div>
+          <div className="flex justify-between">
+            <div className="bg-gray-600 p-2 m-2 rounded-lg max-w-max text-xs">
+              Tmin: {data.main.temp_min.toFixed(1)} °C
+            </div>
+            <div className="bg-gray-600 p-2 m-2 rounded-lg max-w-max text-xs">
+              Tmax: {data.main.temp_max.toFixed(1)} °C
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-700 p-2 m-2 rounded-lg">
+          <div className="bg-gray-600 p-2 m-2 rounded-lg uppercase">
+            {data.weather[0].description}
+          </div>
+          <div className="bg-gray-600 p-2 m-2 rounded-lg">
+            Ощущается: {Math.round(data.main.feels_like)} °C
+          </div>
+        </div>
+        {/* <div className="bg-gray-700 p-2 m-2 rounded-lg">
+          <div className="bg-gray-600 p-2 m-2 rounded-lg">
+            Атмосферное давление: {data.main.pressure} гПа
+          </div>
+          <div className="bg-gray-600 p-2 m-2 rounded-lg">
+            Относительная влажность: {data.main.humidity}%
+          </div>
+        </div> */}
+        <div className="bg-gray-700 p-2 m-2 rounded-lg">
+          <div className="bg-gray-600 p-2 m-2 rounded-lg">
+            Видимость: {data.visibility} м
+          </div>
+          <div className="bg-gray-600 p-2 m-2 rounded-lg">
+            Скорость ветра: {data.wind.speed} м/сек
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Weather;
